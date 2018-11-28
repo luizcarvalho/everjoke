b4aca6310dd80aad9c01d254001fca9e91de768d
Luiz Carvalho <maximusmano@gmail.com>
2018-11-28 11:48:26 -0300

    restructuring project and add pwa

---
+++ docs/scripts/app.js
@@ -0,0 +1,115 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+(function() {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    spinner: document.querySelector('.loader'),
+    cardTemplate: document.querySelector('.cardTemplate'),
+    container: document.querySelector('.main'),
+    api_url: 'https://raw.githubusercontent.com/luizcarvalho/everjoke/master/api/v1/jokes.json'
+  };
+
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  document.getElementById('butRefresh').addEventListener('click', function() {
+    // Refresh all of the forecasts
+    app.getJokes();
+  });
+
+
+  /*****************************************************************************
+   *
+   * Methods to update/refresh the UI
+   *
+   ****************************************************************************/
+
+  // Iterate all of the cards and attempt to get the latest forecast data
+  // app.updateForecasts = function() {
+  //   app.getJokes();
+  // };
+
+  app.updateJokes = function(jokes) {
+    jokes.forEach(app.updateJokeCard)
+  };
+
+
+  // Updates a weather card with the latest weather forecast. If the card
+  // doesn't already exist, it's cloned from the template.
+  app.updateJokeCard = function(joke) {
+      var card = app.cardTemplate.cloneNode(true);
+      card.classList.remove('cardTemplate');
+      card.querySelector('.question').textContent = joke.question;
+      card.querySelector('.answer').textContent = joke.answer;
+      card.removeAttribute('hidden');
+      app.container.appendChild(card);
+
+    if (app.isLoading) {
+      app.spinner.setAttribute('hidden', true);
+      app.container.removeAttribute('hidden');
+      app.isLoading = false;
+    }
+  };
+
+
+  /*****************************************************************************
+   *
+   * Methods for dealing with the model
+   *
+   ****************************************************************************/
+
+  /*
+   * Gets a forecast for a specific city and updates the card with the data.
+   * getJokes() first checks if the weather data is in the cache. If so,
+   * then it gets that data and populates the card with the cached data.
+   * Then, getJokes() goes to the network for fresh data. If the network
+   * request goes through, then the card gets updated a second time with the
+   * freshest data.
+   */
+  app.getJokes = function() {
+    var url = 'https://raw.githubusercontent.com/luizcarvalho/everjoke/master/v1/jokes.json';
+    // TODO add cache logic here
+    console.log('antes de fazer a questest')
+
+    // Fetch the latest data.
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          console.log(response);
+          var jokes = response.jokes;
+          app.updateJokes(jokes);
+        }
+      }
+    };
+    request.open('GET', url);
+    request.send();
+  };
+
+  // TODO uncomment line below to test app with fake data
+  app.getJokes();
+
+  // TODO add startup code here
+
+  // TODO add service worker code here
+})();
